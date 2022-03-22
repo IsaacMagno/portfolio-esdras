3d3336390b37948a47d8534bea160a2bc7549117
IsaacMagno <isaacmagno98@gmail.com>
2022-03-22 13:57:50 -0300

    first commit

---
+++ JavaScript/script.js
@@ -0,0 +1,26 @@
+$('.main-slider').slick({
+  dots: true,
+  centerMode: true,
+  centerPadding: '80px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
